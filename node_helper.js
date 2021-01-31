1b1a1a0bb93460ed5476a62ffea8a9dffde978c8
FatTony404 <FatTony123@users.noreply.github.com>
2021-01-31 22:05:45 +0100

    dev

--- node_helper.js
+++ node_helper.js
@@ -94,7 +94,7 @@ function parseEntry(row) {
 	var td6 = td5.next();
 	// var td7 = td6.next();
 
-	console.log("row: "+td6.val());
+	console.log("row: "+td6.val('data-value'));
 
 	entry.skiarea = td1.text().trim();
 	entry.tal = td2.text().trim();
